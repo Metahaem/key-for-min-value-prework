fa4c6611b9688ff86745469db1c3c72bef628973
Tom Rowson <tom.rowson@gmail.com>
2018-10-23 16:03:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-new_hash = name_hash.map {|key, value|
+reject_hash = {}
+good_hash =
+name_hash.map {|key, value|
 if value == value.smallest
   new_hash << key
 new_hash
