e7286753fbf3794693d552e32c6a64ded4566a2b
Tom Rowson <tom.rowson@gmail.com>
2018-10-23 16:57:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
 smallest_value = []
 smallest_hash = {}
 name_hash.each {|key, value|
-if smallest_value.empty? == TRUE
+if smallest_value.empty? == true
   smallest_value << value
   smallest_key << key
 end
